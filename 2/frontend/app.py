fea289950992e75ad7f656c7455c5851257c43d8
Chinmay Mahapatra <chinmaymahapatra6@gmail.com>
2025-08-21 17:01:06 +0530

    Create app.py

---
+++ 2/frontend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+BACKEND_URL = 'http://127.0.0.1:8000'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit',  methods=['POST'])
+def submit():
+    form_data = dict(request.form)
+    requests.post(BACKEND_URL + '/submit', json=form_data)
+
+    return "Data submitted successfully"
+
+if __name__=='__main__':
+    app.run(debug=True)
