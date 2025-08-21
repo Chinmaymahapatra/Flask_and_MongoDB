43817b6c73265c910ece19cbeac7a122a2257648
Chinmay Mahapatra <chinmaymahapatra6@gmail.com>
2025-08-21 17:08:43 +0530

    Update app.py

--- 2/frontend/app.py
+++ 2/frontend/app.py
@@ -12,9 +12,11 @@ def home():
 @app.route('/submit',  methods=['POST'])
 def submit():
     form_data = dict(request.form)
-    requests.post(BACKEND_URL + '/submit', json=form_data)
-
-    return "Data submitted successfully"
+    try:
+        requests.post(BACKEND_URL + '/submit', json=form_data)
+        return "Data submitted successfully"
+    except Exception as e:
+        return e
 
 if __name__=='__main__':
     app.run(debug=True)
